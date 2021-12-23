4ba3de9c7bf24169cce61ae622404b9be1144c80
Tomoki-sic <cy17237@shibaura-it.ac.jp>
2021-12-24 00:16:37 +0900

    django_setup

---
+++ line_bot/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+import json
+from django.views.decorators.csrf import csrf_exempt
+
+# Create your views here.
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+    
